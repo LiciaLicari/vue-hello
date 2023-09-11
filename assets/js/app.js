b838c4cae3d17b5eb94845c3e5b96edb0b32633d
“LiciaLicari” <licianbsplicari@gmail.com>
2023-09-11 13:09:43 +0200

    add title + img

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+/* Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa inserendo il percoso in una propietá dell'oggetto restituito dalla funzione data. */
+
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+      message: "Hello darkness my old friend",
+      bonusImg: "https://picsum.photos/200/300"
+    
+    }
+    
+  },
+  
+}).mount('#app');
